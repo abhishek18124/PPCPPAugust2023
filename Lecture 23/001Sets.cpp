3a9acaca9e07eda9481ec19abe42b6ff57dcb38a
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-11-25 19:48:35 +0530

    Lecture 23

--- Lecture 23/001Sets.cpp
+++ Lecture 23/001Sets.cpp
@@ -1,8 +1,8 @@
 /*
-	
+
 	https://cplusplus.com/reference/set/set/?kw=set
 	https://cplusplus.com/reference/set/multiset/?kw=multiset
-	
+
 	> insert/erase/find is logarithmic i.e. O(logn)
 	> values are inserted in the sorted order
 	> set contains only distinct values
@@ -18,7 +18,7 @@ using namespace std;
 int main() {
 
 	set<int> s;
-	
+
 	s.insert(5);
 	s.insert(4);
 	s.insert(3);
@@ -32,25 +32,25 @@ int main() {
 
 	cout << "size : " << s.size() << endl;
 
-	for(auto it=s.begin(), end=s.end(); it != end; it++) {
+	for (auto it = s.begin(), end = s.end(); it != end; it++) {
 		cout << *it << " ";
 	}
 
 	cout << endl;
 
-	for(auto x : s) {
+	for (auto x : s) {
 		cout << x << " ";
 	}
 
 	cout << endl;
 
-	if(s.find(3) != s.end()) {
+	if (s.find(3) != s.end()) {
 		cout << "3 is present" << endl;
 	} else {
 		cout << "3 is absent" << endl;
 	}
 
-	if(s.count(5)) {
+	if (s.count(5)) {
 		cout << "5 is present" << endl;
 	} else {
 		cout << "5 is absent" << endl;
