1fe8e831c58bb46fe94ee1896f20c888ca6d4b09
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-12-16 19:36:22 +0530

    Lecture 27

--- Lecture 27/005LinkedList_KthNodeFromLast.cpp
+++ Lecture 27/005LinkedList_KthNodeFromLast.cpp
@@ -3,49 +3,54 @@
 using namespace std;
 
 class ListNode {
+public:
 
-	public :
-		
-		int val;
-		ListNode* next;
+	int val;
+	ListNode* next;
 
-		ListNode(int val) {
-			this->val = val;
-			this->next = NULL;
-		}
+	ListNode(int val) {
+		this->val = val;
+		this->next = NULL;
+	}
+
 };
 
 void insertAtHead(ListNode*& head, int val) {
-	
+
 	ListNode* n = new ListNode(val);
 	n->next = head;
 	head = n;
 
 }
 
+
 void printLinkedList(ListNode* head) {
 
-	while(head != NULL) {
+	while (head) { // head != NULL
 		cout << head->val << " ";
 		head = head->next;
 	}
-	
-	cout << endl;
 
+	cout << endl;
 }
 
 int main() {
 
-	ListNode* head = NULL; // initially, linkedList is empty
+	ListNode* head = NULL;
 
+	insertAtHead(head, 70);
+	insertAtHead(head, 60);
 	insertAtHead(head, 50);
 	insertAtHead(head, 40);
 	insertAtHead(head, 30);
 	insertAtHead(head, 20);
 	insertAtHead(head, 10);
-	insertAtHead(head, 0);
 
 	printLinkedList(head);
+
+	int k = 2;
+
+	// todo ...
 
 	return 0;
 }
